3728ccae791796d021fbcc5004b1275e830ab120
IGE-123016 <tfsps@iscte-iul.pt>
2026-02-27 19:59:44 +0000

    JavaDoc pages generated
    JavaDoc pages generated for Barge.java, Caravel.java, Game.java (regenerated - small correction) and Position.java

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"iscteiul.ista.battleship","l":"Game"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"iscteiul.ista.battleship","l":"Barge"},{"p":"iscteiul.ista.battleship","l":"Caravel"},{"p":"iscteiul.ista.battleship","l":"Game"},{"p":"iscteiul.ista.battleship","l":"Position"}];updateSearchResults();
