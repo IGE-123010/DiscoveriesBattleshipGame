5a54c9b9192a025fe086c59329756136b91df683
IGE-123010 <mssae@iscte-iul.pt>
2026-02-28 21:04:52 +0000

    update toString Ship.java

--- src/main/java/iscteiul/ista/battleship/Ship.java
+++ src/main/java/iscteiul/ista/battleship/Ship.java
@@ -255,6 +255,6 @@ public abstract class Ship implements IShip {
      */
     @Override
     public String toString() {
-        return "[" + category + " facing " + bearing + "]";
+        return "[" + category + "|" + bearing + "|" + pos + "]";
     }
 }
